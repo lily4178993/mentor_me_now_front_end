65b9d195e34fc668dca9822f737a598c4b282579
Martin Karugaba <martinkarugaba21@gmail.com>
2024-02-05 11:33:40 +0300

    Fix linter errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
-import './App.css'
+import './App.css';
 
 function App() {
-
   return (
     <>
-     <h1 className='text-3xl text-primary-green font-medium'>Mentor me now front end</h1>
-     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus iste harum unde? Nihil est voluptates deleniti libero, ipsa quidem perspiciatis quam vero fuga dolores repellat praesentium earum et cupiditate! Consectetur nulla soluta ea ipsam obcaecati ullam necessitatibus, consequuntur perspiciatis, eligendi officiis ducimus, maiores aliquam eaque nisi ex odio delectus et? </p>
+      <h1 className="text-3xl text-primary-green font-medium">
+        Mentor me now front end
+      </h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus iste
+        harum unde? Nihil est voluptates deleniti libero, ipsa quidem
+        perspiciatis quam vero fuga dolores repellat praesentium earum et
+        cupiditate! Consectetur nulla soluta ea ipsam obcaecati ullam
+        necessitatibus, consequuntur perspiciatis, eligendi officiis ducimus,
+        maiores aliquam eaque nisi ex odio delectus et?
+        {' '}
+      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       colors: {
-        "primary-green": "#97BF0F",
-        "primary-orange": "#FFB400",
-        "primary-blue": "#10BBB5",
-        "primary-gray": "#BCBCBE",
-        "primary-black": "#252525",
-        "primary-red": "#B32A31",
+        'primary-green': '#97BF0F',
+        'primary-orange': '#FFB400',
+        'primary-blue': '#10BBB5',
+        'primary-gray': '#BCBCBE',
+        'primary-black': '#252525',
+        'primary-red': '#B32A31',
       },
       fontFamily: {
-        pacifico: ["Pacifico", "cursive"],
+        pacifico: ['Pacifico', 'cursive'],
       },
     },
   },
