586a06ca58c84b82d74daa0102f9f54fdd52f7cc
Martin Karugaba <martinkarugaba21@gmail.com>
2024-02-05 11:25:41 +0300

    Set up custom fonts and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,19 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-green": "#97BF0F",
+        "primary-orange": "#FFB400",
+        "primary-blue": "#10BBB5",
+        "primary-gray": "#BCBCBE",
+        "primary-black": "#252525",
+        "primary-red": "#B32A31",
+      },
+      fontFamily: {
+        pacifico: ["Pacifico", "cursive"],
+      },
+    },
   },
   plugins: [],
 };
-
