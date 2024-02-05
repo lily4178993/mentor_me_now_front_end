7afcd51f1fcd2a0b54694a47c9ed69ced60d492a
Martin Karugaba <martinkarugaba21@gmail.com>
2024-02-05 19:46:34 +0300

    Add authentication

---
+++ src/components/SignInForm.jsx
@@ -0,0 +1,35 @@
+import { useDispatch } from 'react-redux';
+import { setUsername } from '../redux/authSlice';
+
+const SignInForm = () => {
+  const dispatch = useDispatch();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const username = event.target.username.value;
+    dispatch(setUsername(username));
+  };
+
+  return (
+    <div>
+      <h1>Sign In</h1>
+
+      <form onSubmit={handleSubmit}>
+        <div className="border mt-4">
+          <input
+            type="text"
+            className="rounded p-2"
+            name="username"
+            id="username"
+          />
+        </div>
+        <div className="border-2 mt-4">
+          <button type="submit">Sign in</button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default SignInForm;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import App from './App';
-import './index.css';
+import store from './redux/store';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>,
 );
