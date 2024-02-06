8ff15559780cb5ba09d18e7c0fc1abcab228460e
Ayokunnumi1 <omololuayk@gmail.com>
2024-02-06 17:48:31 +0100

    Include Navlinks & activate pages

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,37 @@
-/* eslint-disable arrow-body-style */
-const NavBar = () => {
-  return (
-    <div className="flex flex-col">
-      <h1>Navbar</h1>
-      <h2>Navbar</h2>
-    </div>
-  );
-};
+import { NavLink } from 'react-router-dom';
+import '../modules/NavBar.css';
+
+const NavBar = () => (
+  <nav className="">
+    <div>Logo</div>
+    <ul>
+      <li>
+        <NavLink to="/home" className={({ isActive }) => (isActive ? 'active-box' : '')}>
+          Mentors
+        </NavLink>
+      </li>
+      <li>
+        <NavLink to="/reserveMentor" className={({ isActive }) => (isActive ? 'active-box' : '')}>
+          Reserve Mentor
+        </NavLink>
+      </li>
+      <li>
+        <NavLink to="/reservations" className={({ isActive }) => (isActive ? 'active-box' : '')}>
+          Reservations
+        </NavLink>
+      </li>
+      <li>
+        <NavLink to="/addMentor" className={({ isActive }) => (isActive ? 'active-box' : '')}>
+          Add Mentor
+        </NavLink>
+      </li>
+      <li>
+        <NavLink to="/deleteMentor" className={({ isActive }) => (isActive ? 'active-box' : '')}>
+          Delete Mentor
+        </NavLink>
+      </li>
+    </ul>
+  </nav>
+);
 
 export default NavBar;
