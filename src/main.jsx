f9ba9367f36d483d90adab904f21d005a40fb0f3
Martin Karugaba <martinkarugaba21@gmail.com>
2024-02-06 17:04:31 +0300

    Modify main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import App from './App';
 import store from './redux/store';
+import SignInForm from './components/SignInForm';
 import './index.css';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <App />,
+    element: <SignInForm />,
   },
 ]);
 
