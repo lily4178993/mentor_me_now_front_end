45cdfa8051e612dc3ec1a9c06a72a7a2f8b4bc47
Ayokunnumi1 <omololuayk@gmail.com>
2024-02-06 20:06:03 +0100

    Edit & Update Navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,15 @@
 import { NavLink } from 'react-router-dom';
 import '../modules/NavBar.css';
+import logo from '../assets/logo.jpg';
+import twitter from '../assets/twitter.svg';
+import facebook from '../assets/facebook.svg';
+import google from '../assets/google.svg';
+import instagram from '../assets/instagram.svg';
+import pinterest from '../assets/pinterest.svg';
 
 const NavBar = () => (
   <nav className="navbar">
-    <div>Logo</div>
+    <img src={logo} alt="logo" className="logo" />
     <ul className="navbar_links">
       <li>
         <NavLink to="/home" className={({ isActive }) => (isActive ? 'active_box' : '')}>
@@ -31,6 +37,14 @@ const NavBar = () => (
         </NavLink>
       </li>
     </ul>
+    <div className="social_media">
+      <img src={twitter} alt="twitter logo" />
+      <img src={facebook} alt="facebook logo" />
+      <img src={google} alt="google logo" />
+      <img src={instagram} alt="instagram logo" />
+      <img src={pinterest} alt="pinterest logo" />
+    </div>
+    <p className="copyright_text">&#169;Copyright 2024</p>
   </nav>
 );
 
