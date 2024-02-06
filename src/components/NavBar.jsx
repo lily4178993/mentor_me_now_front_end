cd979d7b206f5f9fe4af3cae7de6f6f72d45f7f3
Ayokunnumi1 <omololuayk@gmail.com>
2024-02-06 18:39:04 +0100

    Update Navbar component & style it

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,32 +2,32 @@ import { NavLink } from 'react-router-dom';
 import '../modules/NavBar.css';
 
 const NavBar = () => (
-  <nav className="">
+  <nav className="navbar">
     <div>Logo</div>
-    <ul>
+    <ul className="navbar_links">
       <li>
-        <NavLink to="/home" className={({ isActive }) => (isActive ? 'active-box' : '')}>
+        <NavLink to="/home" className={({ isActive }) => (isActive ? 'active_box' : '')}>
           Mentors
         </NavLink>
       </li>
       <li>
-        <NavLink to="/reserveMentor" className={({ isActive }) => (isActive ? 'active-box' : '')}>
+        <NavLink to="/reserveMentor" className={({ isActive }) => (isActive ? 'active_box' : '')}>
           Reserve Mentor
         </NavLink>
       </li>
       <li>
-        <NavLink to="/reservations" className={({ isActive }) => (isActive ? 'active-box' : '')}>
+        <NavLink to="/reservations" className={({ isActive }) => (isActive ? 'active_box' : '')}>
           Reservations
         </NavLink>
       </li>
       <li>
-        <NavLink to="/addMentor" className={({ isActive }) => (isActive ? 'active-box' : '')}>
+        <NavLink to="/addMentor" className={({ isActive }) => (isActive ? 'active_box' : '')}>
           Add Mentor
         </NavLink>
       </li>
       <li>
-        <NavLink to="/deleteMentor" className={({ isActive }) => (isActive ? 'active-box' : '')}>
-          Delete Mentor
+        <NavLink to="/delete" className={({ isActive }) => (isActive ? 'active_box' : '')}>
+          Delete
         </NavLink>
       </li>
     </ul>
