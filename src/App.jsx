04933f6acaf826986a607a0429b128eb4209ccf6
Ayokunnumi1 <omololuayk@gmail.com>
2024-02-06 17:47:02 +0100

    Use location hook & import necessary files

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import SignInForm from './components/SignInForm';
 import Home from './pages/Home';
+import ReserveMentor from './components/ReserveMentor';
+import Reservations from './components/Reservations';
+import AddMentor from './components/AddMentor';
+import DeleteMentor from './components/DeleteMentor';
 import NavBar from './components/Navbar';
 
 function App() {
+  const location = useLocation();
+
   return (
     <>
-      <NavBar />
+      {location.pathname !== '/' && <NavBar />}
       <main>
         <Routes>
           <Route path="/" element={<SignInForm />} />
           <Route path="/home" element={<Home />} />
+          <Route path="/reserveMentor" element={<ReserveMentor />} />
+          <Route path="/reservations" element={<Reservations />} />
+          <Route path="/addMentor" element={<AddMentor />} />
+          <Route path="/deleteMentor" element={<DeleteMentor />} />
         </Routes>
       </main>
     </>
