a2f520a0b4032abd05cc3440b243047bbe7eff6b
Ayokunnumi1 <omololuayk@gmail.com>
2024-02-06 14:50:14 +0100

    Add tailwind styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import { Routes, Route } from 'react-router-dom';
 import SignInForm from './components/SignInForm';
 import Home from './pages/Home';
+import NavBar from './components/Navbar';
 
 function App() {
   return (
-    <main>
-      <Routes>
-        <Route path="/" element={<SignInForm />} />
-        <Route path="/home" element={<Home />} />
-      </Routes>
-    </main>
+    <>
+      <NavBar />
+      <main>
+        <Routes>
+          <Route path="/" element={<SignInForm />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </main>
+    </>
   );
 }
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable arrow-body-style */
 const NavBar = () => {
   return (
-    <div>
+    <div className="flex flex-col">
       <h1>Navbar</h1>
       <h2>Navbar</h2>
     </div>
