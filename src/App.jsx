61c85a572997b519f45fd8a74b717e3fbb7f97d5
Ayokunnumi1 <omololuayk@gmail.com>
2024-02-06 07:53:59 +0100

    Include routes for signInForm & Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+import { Routes, Route } from 'react-router-dom';
 import SignInForm from './components/SignInForm';
+import MainPage from './components/mainPage';
 
 function App() {
   return (
-    <>
-      <SignInForm />
-    </>
+    <Routes>
+      <Route path="/" element={<SignInForm />} />
+      <Route path="/mainpage" element={<MainPage />} />
+    </Routes>
   );
 }
 
