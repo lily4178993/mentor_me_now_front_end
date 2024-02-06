09c24a96798242c46795a9a63e073492a4a0cf13
Ayokunnumi1 <omololuayk@gmail.com>
2024-02-06 07:54:34 +0100

    Edit main.jsx to use browser router

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import App from './App';
 import store from './redux/store';
 import './index.css';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-  },
-]);
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router} />
+      <Router>
+        <App />
+      </Router>
     </Provider>
   </React.StrictMode>,
 );
