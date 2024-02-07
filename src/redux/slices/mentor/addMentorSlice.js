9901d72ffb1c47aa67a2735691a22c9951648d0e
Ayokunnumi1 <omololuayk@gmail.com>
2024-02-07 14:45:54 +0100

    Create add mentor slice & include manage extra reducers

--- src/redux/slices/mentor/addMentorSlice.js
+++ src/redux/slices/mentor/addMentorSlice.js
@@ -1,6 +1,34 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const initialState = {
+  addMentor: [],
+  loading: false,
+  error: '',
+};
+
+const baseUrl = 'http://127.0.0.1:3000';
+
+export const addMentorToServer = createAsyncThunk('addMentor/addToServer', async (obj) => {
+  try {
+    const response = axios.post(`${baseUrl}/api/v1/mentors`, obj);
+    const data = response.json();
+    return data;
+  } catch (error) {
+    return error;
+  }
+});
 
 const addMentorSlice = createSlice({
   name: 'addMentor',
-
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(addMentorToServer.fulfilled, (state, action) => {
+        state.addMentor.push(action.payload);
+      });
+  },
 });
+
+export default addMentorSlice.reducer;
