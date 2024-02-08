4773876ce3983e81dee71c9458ece9ae68673340
Ayokunnumi1 <omololuayk@gmail.com>
2024-02-08 15:17:19 +0100

    Add more jsx tags

--- src/components/AddMentor.jsx
+++ src/components/AddMentor.jsx
@@ -41,73 +41,68 @@ const AddMentor = () => {
   };
 
   return (
-    <form className="add_mentor_form">
-      <div className="form-row">
-        <label htmlFor="name" className="form-label">
-          Name
-          {' '}
-          <input type="text" name="name" value={formData.name} onChange={handleChange} />
-        </label>
-      </div>
-
-      <div className="form-row">
-        <label htmlFor="occupation" className="form-label">
-          {' '}
-          Occupation
-          <input type="text" name="occupation" value={formData.occupation} onChange={handleChange} />
-        </label>
-      </div>
-
-      <div className="form-row">
-        <label htmlFor="about" className="form-label">
-          {' '}
-          About
-          <input type="text" name="about" value={formData.about} onChange={handleChange} />
-        </label>
-      </div>
-
-      <div className="form-row">
-        <label htmlFor="hourly_fee" className="form-label">
-          {' '}
-          Hourly fee
-          <input type="decimal" name="hourly_fee" step="0.01" value={formData.hourly_fee} onChange={handleChange} />
-        </label>
-      </div>
-
-      <div className="form-row">
-        <label htmlFor="year_of_experience" className="form-label">
-          {' '}
-          Year of experience
-          <input type="number" name="year_of_experience" value={formData.year_of_experience} onChange={handleChange} />
-        </label>
-      </div>
-
-      <div className="form-row">
-        <label htmlFor="location" className="form-label">
-          {' '}
-          location
-          <input type="text" name="location" value={formData.location} onChange={handleChange} />
-        </label>
-      </div>
-
-      <div className="form-row">
-        <label htmlFor="skills" className="form-label">
-          {' '}
-          Skills
-          <input type="text" name="skills" value={formData.skills} onChange={handleChange} />
-        </label>
-      </div>
-
-      <div className="form-row">
-        <label htmlFor="photo_url" className="form-label">
-          {' '}
-          Photo
-          <input type="text" name="photo_url" value={formData.photo_url} onChange={handleChange} />
-        </label>
-      </div>
-
-      <button type="submit" onClick={handleSubmit}>Submit</button>
-    </form>
+    <section className="add_mentor_section">
+      <form className="add_mentor_form">
+        <h2>BE A MENTOR</h2>
+        <div className="form-row">
+          <label htmlFor="name" className="form-label">
+            <span className="add_mentor_label">Name</span>
+            {' '}
+            <input type="text" name="name" value={formData.name} onChange={handleChange} />
+          </label>
+        </div>
+        <div className="form-row">
+          <label htmlFor="occupation" className="form-label">
+            {' '}
+            <span className="add_mentor_label">Occupation</span>
+            <input type="text" name="occupation" value={formData.occupation} onChange={handleChange} />
+          </label>
+        </div>
+        <div className="form-row">
+          <label htmlFor="about" className="form-label">
+            {' '}
+            <span className="add_mentor_label">About</span>
+            <input type="text" name="about" value={formData.about} onChange={handleChange} />
+          </label>
+        </div>
+        <div className="form-row">
+          <label htmlFor="hourly_fee" className="form-label">
+            {' '}
+            <span className="add_mentor_label">Hourly fee</span>
+            <input type="number" name="hourly_fee" value={formData.hourly_fee} onChange={handleChange} />
+          </label>
+        </div>
+        <div className="form-row">
+          <label htmlFor="year_of_experience" className="form-label">
+            {' '}
+            <span className="add_mentor_label">Experience</span>
+            <input type="number" name="year_of_experience" value={formData.year_of_experience} onChange={handleChange} />
+          </label>
+        </div>
+        <div className="form-row">
+          <label htmlFor="location" className="form-label">
+            {' '}
+            <span className="add_mentor_label">Location</span>
+            <input type="text" name="location" value={formData.location} onChange={handleChange} />
+          </label>
+        </div>
+        <div className="form-row">
+          <label htmlFor="skills" className="form-label">
+            {' '}
+            <span className="add_mentor_label">Skills</span>
+            <input type="text" name="skills" value={formData.skills} onChange={handleChange} />
+          </label>
+        </div>
+        <div className="form-row">
+          <label htmlFor="photo_url" className="form-label">
+            {' '}
+            <span className="add_mentor_label">Photo</span>
+            <input type="text" name="photo_url" value={formData.photo_url} onChange={handleChange} />
+          </label>
+        </div>
+        <button type="submit" onClick={handleSubmit} className="submit-button">Submit</button>
+      </form>
+    </section>
   );
 };
 
