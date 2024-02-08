1334fc875e0978254f110db5e087465a4bd598d2
Martin Karugaba <martinkarugaba21@gmail.com>
2024-02-08 09:59:19 +0300

    Put mentor details into an array and loop over it

--- src/components/MentorDetails.jsx
+++ src/components/MentorDetails.jsx
@@ -29,14 +29,43 @@ const MentorDetails = () => {
 
   const {
     name,
-    photo_url: photoUrl,
     occupation,
     skills,
-    year_of_experience: yearsOfExperience,
     location,
     about,
+    photo_url: photoUrl,
+    year_of_experience: yearsOfExperience,
     hourly_fee: hourlyFee,
   } = details;
+
+  const mentorDetails = [
+    {
+      id: 1,
+      title: 'Location',
+      value: location,
+    },
+    {
+      id: 2,
+      title: 'Hourly rate',
+      value: `$${hourlyFee}/hr`,
+    },
+    {
+      id: 3,
+      title: 'Experience',
+      value: `${yearsOfExperience} yrs`,
+    },
+    {
+      id: 4,
+      title: 'Occupation',
+      value: occupation,
+    },
+    {
+      id: 5,
+      title: 'Skills',
+      value: skills,
+    },
+  ];
+
   return (
     <section className="flex border w-full border-orange-500 justify-between items-start">
       <div className="border flex justify-center items-center basis-2/3 h-[90%]">
@@ -48,37 +77,18 @@ const MentorDetails = () => {
       </div>
       <div className="border basis-1/3 pt-[5rem]">
         <header className="px-4 text-right">
-          <h2 className="text-4xl font-medium">{name}</h2>
-          <p className="mt-4">{about}</p>
+          <h2 className="text-4xl text-primary-black font-medium">{name}</h2>
+          <p className="mt-4 text-gray-700">{about}</p>
         </header>
         <section className="mt-8">
-          <div className="flex odd:bg-gray-200  justify-between items-center py-4 px-4">
-            <p>Location</p>
-            <p>{location}</p>
-          </div>
-          <div className="flex odd:bg-gray-200  justify-between items-center py-4 px-4">
-            <p>Hourly rate</p>
-            <p>
-              $
-              {hourlyFee}
-              /hr
-            </p>
-          </div>
-          <div className="flex odd:bg-gray-200 justify-between items-center py-4 px-4">
-            <p>Experience</p>
-            <p>
-              {yearsOfExperience}
-              yrs
-            </p>
-          </div>
-          <div className="flex odd:bg-gray-200 justify-between items-center py-4 px-4">
-            <p>Occupation</p>
-            <p>{occupation}</p>
-          </div>
-          <div className="flex odd:bg-gray-200 justify-between items-center py-4 px-4">
-            <p>Skills</p>
-            <p>{skills}</p>
-          </div>
+          {
+            mentorDetails.map((detail) => (
+              <div key={detail.id} className="flex odd:bg-gray-200  justify-between items-center py-4 px-4">
+                <p className="font-medium">{detail.title}</p>
+                <p className="text-gray-700">{detail.value}</p>
+              </div>
+            ))
+          }
         </section>
       </div>
     </section>
