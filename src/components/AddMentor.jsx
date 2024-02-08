75eb0e4056e70b5a984db0e79fc9750146e9fede
Ayokunnumi1 <omololuayk@gmail.com>
2024-02-08 13:42:40 +0100

    Update Add mentor component

--- src/components/AddMentor.jsx
+++ src/components/AddMentor.jsx
@@ -28,57 +28,83 @@ const AddMentor = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     dispatch(addMentorToServer(formData));
+    setFormData({
+      name: '',
+      occupation: '',
+      about: '',
+      hourly_fee: 0,
+      year_of_experience: 0,
+      location: '',
+      skills: '',
+      photo_url: '',
+    });
   };
 
   return (
     <form className="add_mentor_form">
-      <label htmlFor="name" className="form-label">
-        Name
-        {' '}
-        <input type="text" name="name" value={formData.name} onChange={handleChange} />
-      </label>
+      <div className="form-row">
+        <label htmlFor="name" className="form-label">
+          Name
+          {' '}
+          <input type="text" name="name" value={formData.name} onChange={handleChange} />
+        </label>
+      </div>
 
-      <label htmlFor="occupation" className="form-label">
-        {' '}
-        Occupation
-        <input type="text" name="occupation" value={formData.occupation} onChange={handleChange} />
-      </label>
+      <div className="form-row">
+        <label htmlFor="occupation" className="form-label">
+          {' '}
+          Occupation
+          <input type="text" name="occupation" value={formData.occupation} onChange={handleChange} />
+        </label>
+      </div>
 
-      <label htmlFor="about" className="form-label">
-        {' '}
-        About
-        <input type="text" name="about" value={formData.about} onChange={handleChange} />
-      </label>
+      <div className="form-row">
+        <label htmlFor="about" className="form-label">
+          {' '}
+          About
+          <input type="text" name="about" value={formData.about} onChange={handleChange} />
+        </label>
+      </div>
 
-      <label htmlFor="shourly_fee" className="form-label">
-        {' '}
-        Hourly fee
-        <input type="decimal" name="hourly_fee" step="0.01" value={formData.hourly_fee} onChange={handleChange} />
-      </label>
+      <div className="form-row">
+        <label htmlFor="hourly_fee" className="form-label">
+          {' '}
+          Hourly fee
+          <input type="decimal" name="hourly_fee" step="0.01" value={formData.hourly_fee} onChange={handleChange} />
+        </label>
+      </div>
 
-      <label htmlFor="year_of_experience" className="form-label">
-        {' '}
-        Year of experience
-        <input type="number" name="year_of_experience" value={formData.year_of_experience} onChange={handleChange} />
-      </label>
+      <div className="form-row">
+        <label htmlFor="year_of_experience" className="form-label">
+          {' '}
+          Year of experience
+          <input type="number" name="year_of_experience" value={formData.year_of_experience} onChange={handleChange} />
+        </label>
+      </div>
 
-      <label htmlFor="location" className="form-label">
-        {' '}
-        location
-        <input type="text" name="location" value={formData.location} onChange={handleChange} />
-      </label>
+      <div className="form-row">
+        <label htmlFor="location" className="form-label">
+          {' '}
+          location
+          <input type="text" name="location" value={formData.location} onChange={handleChange} />
+        </label>
+      </div>
 
-      <label htmlFor="skills" className="form-label">
-        {' '}
-        skills
-        <input type="text" name="skills" value={formData.skills} onChange={handleChange} />
-      </label>
+      <div className="form-row">
+        <label htmlFor="skills" className="form-label">
+          {' '}
+          Skills
+          <input type="text" name="skills" value={formData.skills} onChange={handleChange} />
+        </label>
+      </div>
 
-      <label htmlFor="photo_url" className="form-label">
-        {' '}
-        Photo
-        <input type="text" name="photo_url" value={formData.photo_url} onChange={handleChange} />
-      </label>
+      <div className="form-row">
+        <label htmlFor="photo_url" className="form-label">
+          {' '}
+          Photo
+          <input type="text" name="photo_url" value={formData.photo_url} onChange={handleChange} />
+        </label>
+      </div>
 
       <button type="submit" onClick={handleSubmit}>Submit</button>
     </form>
