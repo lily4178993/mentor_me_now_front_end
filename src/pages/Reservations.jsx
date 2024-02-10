129e6e1985c95d923a054d9d3a4f60e2b54acfb2
lily4178993 <nellytelli@gmail.com>
2024-02-10 03:28:07 +0000

    feat: add AlertStatus component to Reservations page

--- src/pages/Reservations.jsx
+++ src/pages/Reservations.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchReservationsList } from '../redux/slices/reservations/reservationsListSlice';
 import LoadingStatus from '../components/ux/LoadingStatus';
+import AlertStatus from '../components/AlertStatus';
 
 const calculateFees = (startTimeParam, endTimeParam, hourlyFeeParam) => {
   const convertTo24Hour = (time) => {
@@ -38,15 +39,24 @@ const Reservations = () => {
 
   return (
     <section className="w-full h-full m-auto px-2 md:p-2 relative overflow-hidden">
-      <h1 className="text-center text-4xl font-bold underline p-4 mb-3">Your Appointments</h1>
-      {loading && <div className="absolute w-full h-full z-10 top-0 left-0 backdrop-blur-sm bg-primary-black/50 flex items-center justify-center"><LoadingStatus /></div>}
+      <h1 className="text-center text-4xl font-bold underline p-4 mb-3">
+        Your Appointments
+      </h1>
+      {loading && (
+        <div className="absolute w-full h-full z-10 top-0 left-0 backdrop-blur-sm bg-primary-black/50 flex items-center justify-center">
+          <LoadingStatus />
+        </div>
+      )}
       {error && <p>{error}</p>}
       {reservations && reservations.length > 0 ? (
         <ul className="flex flex-wrap justify-center gap-x-2 gap-y-3 md:gap-y-5">
           {reservations.map((reservation) => {
             const mentor = mentors.find((m) => m.id === reservation.mentor_id);
             return (
-              <li key={reservation.id} className="min-w-64 bg-primary-green shadow-2xl rounded-md">
+              <li
+                key={reservation.id}
+                className="min-w-64 bg-primary-green shadow-2xl rounded-md"
+              >
                 {mentor ? (
                   <div className="flex flex-col">
                     <img
@@ -55,22 +65,30 @@ const Reservations = () => {
                       className="max-h-32 mx-auto"
                     />
                     <div className="text-xs text-white p-2 relative">
-                      <span className="text-xs text-center py-2 block">Meeting</span>
+                      <span className="text-xs text-center py-2 block">
+                        Meeting
+                      </span>
                       <p className="flex justify-between">
                         <span className="font-extrabold">With </span>
                         <span className="text-right italic">{mentor.name}</span>
                       </p>
                       <p className="flex justify-between">
                         <span className="font-extrabold">On </span>
-                        <span className="text-right italic">{reservation.date}</span>
+                        <span className="text-right italic">
+                          {reservation.date}
+                        </span>
                       </p>
                       <p className="flex justify-between">
                         <span className="font-extrabold">From </span>
-                        <span className="text-right italic">{reservation.formatted_times.start_time}</span>
+                        <span className="text-right italic">
+                          {reservation.formatted_times.start_time}
+                        </span>
                       </p>
                       <p className="flex justify-between">
                         <span className="font-extrabold">To </span>
-                        <span className="text-right italic">{reservation.formatted_times.end_time}</span>
+                        <span className="text-right italic">
+                          {reservation.formatted_times.end_time}
+                        </span>
                       </p>
                       <p className="flex justify-between">
                         <span className="font-extrabold">Total fees </span>
@@ -87,8 +105,11 @@ const Reservations = () => {
                   </div>
                 ) : (
                   <p>
-                    We couldn`t find a mentor matching your request. It seems
-                    like you haven`t reserved a mentor in the usual manner.
+                    <AlertStatus
+                      alertMessage="We couldn`t find a mentor matching your request. It seems
+                    like you haven`t reserved a mentor in the usual manner."
+                      className="absolute w-full h-full z-10 top-0 left-0 backdrop-blur-sm bg-primary-black/50 flex items-center justify-center"
+                    />
                   </p>
                 )}
               </li>
@@ -97,8 +118,11 @@ const Reservations = () => {
         </ul>
       ) : (
         <p>
-          We couldn`t find any reservations. It looks like you haven`t reserved
-          any mentors yet.
+          <AlertStatus
+            alertMessage="We couldn`t find any reservations. It looks like you haven`t reserved
+          any mentors yet."
+            className="absolute w-full h-full z-10 top-0 left-0 backdrop-blur-sm bg-primary-black/50 flex items-center justify-center"
+          />
         </p>
       )}
     </section>
