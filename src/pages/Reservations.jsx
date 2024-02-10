272e6ecb23ed6fc3b7256e2da02e1d1ff06d5191
lily4178993 <nellytelli@gmail.com>
2024-02-10 02:58:41 +0000

    style: Refactor Reservations component UI

--- src/pages/Reservations.jsx
+++ src/pages/Reservations.jsx
@@ -26,47 +26,51 @@ const Reservations = () => {
   }
 
   return (
-    <>
-      <h1>Reservations List:</h1>
+    <section className="w-full h-full m-auto px-2 md:p-2">
+      <h1 className="text-center text-4xl font-bold underline p-4 mb-3">Your Appointments</h1>
       {loading && <p>Loading...</p>}
       {error && <p>{error}</p>}
       {reservations && reservations.length > 0 ? (
-        <ul>
+        <ul className="flex flex-wrap justify-center gap-x-2 gap-y-3 md:gap-y-5">
           {reservations.map((reservation) => {
             const mentor = mentors.find((m) => m.id === reservation.mentor_id);
             return (
-              <li key={reservation.id}>
+              <li key={reservation.id} className="min-w-64 bg-primary-green shadow-2xl rounded-md">
                 {mentor ? (
-                  <div className="flex">
+                  <div className="flex flex-col">
                     <img
                       src={mentor.photo_url}
                       alt=""
-                      className="max-h-28"
+                      className="max-h-32 mx-auto"
                     />
-                    <div>
-                      <p>
-                        <span>Mentor: </span>
-                        {mentor.name}
+                    <div className="text-xs text-white p-2 relative">
+                      <span className="text-xs text-center py-2 block">Meeting</span>
+                      <p className="flex justify-between">
+                        <span className="font-extrabold">With </span>
+                        <span className="text-right italic">{mentor.name}</span>
                       </p>
-                      <p>
-                        <span>Appointment Date: </span>
-                        {reservation.date}
+                      <p className="flex justify-between">
+                        <span className="font-extrabold">On </span>
+                        <span className="text-right italic">{reservation.date}</span>
                       </p>
-                      <p>
-                        <span>Start time: </span>
-                        {reservation.formatted_times.start_time}
+                      <p className="flex justify-between">
+                        <span className="font-extrabold">From </span>
+                        <span className="text-right italic">{reservation.formatted_times.start_time}</span>
                       </p>
-                      <p>
-                        <span>End time: </span>
-                        {reservation.formatted_times.end_time}
+                      <p className="flex justify-between">
+                        <span className="font-extrabold">To </span>
+                        <span className="text-right italic">{reservation.formatted_times.end_time}</span>
                       </p>
-                      <p>
-                        Total fees: $
-                        {calculateFees(
-                          reservation.formatted_times.start_time,
-                          reservation.formatted_times.end_time,
-                          mentor.hourly_fee,
-                        )}
+                      <p className="flex justify-between">
+                        <span className="font-extrabold">Total fees </span>
+                        <span className="text-right italic">
+                          $
+                          {calculateFees(
+                            reservation.formatted_times.start_time,
+                            reservation.formatted_times.end_time,
+                            mentor.hourly_fee,
+                          )}
+                        </span>
                       </p>
                     </div>
                   </div>
@@ -86,7 +90,7 @@ const Reservations = () => {
           any mentors yet.
         </p>
       )}
-    </>
+    </section>
   );
 };
 
