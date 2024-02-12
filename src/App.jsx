8b6813e2ee5bf3d3714d9ad45257126e68de0a3c
lily4178993 <nellytelli@gmail.com>
2024-02-12 21:04:10 +0000

    feat: refactor authentication routes and add navigation based on authentication status

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,10 @@
-import { Routes, Route, useLocation } from 'react-router-dom';
+import React from 'react';
+import {
+  Routes, Route, Navigate,
+} from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import SplashScreen from './pages/SplashScreen';
+import Login from './pages/Login';
 import SignInForm from './components/SignInForm';
 import Home from './pages/Home';
 import Reservations from './pages/Reservations';
@@ -10,21 +16,23 @@ import RemoveMentorsPage from './pages/RemoveMentorsPage';
 import RemovedMentorsList from './pages/RemovedMentorsList';
 
 function App() {
-  const location = useLocation();
+  const isAuthenticated = useSelector((state) => state.authLogin.isAuthenticated);
 
   return (
     <main className="w-full h-auto md:h-[100vh] flex justify-start items-center border-blue-500">
-      {location.pathname !== '/' && <NavBar />}
+      { isAuthenticated && <NavBar /> }
       <section className="h-full lg:w-[80%] flex">
         <Routes>
-          <Route path="/" element={<SignInForm />} />
-          <Route path="/mentors" element={<Home />} />
-          <Route path="/mentors/:id" element={<MentorDetails />} />
-          <Route path="/reserveMentor" element={<ReserveMentor />} />
-          <Route path="/reservations" element={<Reservations />} />
-          <Route path="/addMentor" element={<AddMentor />} />
-          <Route path="/remove_mentor" element={<RemoveMentorsPage />} />
-          <Route path="/removed_mentors" element={<RemovedMentorsList />} />
+          <Route path="/" element={isAuthenticated ? <Navigate to="/mentors" /> : <SplashScreen />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signin" element={<SignInForm />} />
+          <Route path="/mentors" element={isAuthenticated ? <Home /> : <Navigate to="/login" />} />
+          <Route path="/mentors/:id" element={isAuthenticated ? <MentorDetails /> : <Navigate to="/login" />} />
+          <Route path="/reserveMentor" element={isAuthenticated ? <ReserveMentor /> : <Navigate to="/login" />} />
+          <Route path="/reservations" element={isAuthenticated ? <Reservations /> : <Navigate to="/login" />} />
+          <Route path="/addMentor" element={isAuthenticated ? <AddMentor /> : <Navigate to="/login" />} />
+          <Route path="/remove_mentor" element={isAuthenticated ? <RemoveMentorsPage /> : <Navigate to="/login" />} />
+          <Route path="/removed_mentors" element={isAuthenticated ? <RemovedMentorsList /> : <Navigate to="/login" />} />
         </Routes>
       </section>
     </main>
