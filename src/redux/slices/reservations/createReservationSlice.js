59d83ea9c7d6af9067f73762c1244dd9754a3caa
Ayokunnumi1 <omololuayk@gmail.com>
2024-02-12 13:11:57 +0100

    Add a create reservation slice in the redux folder with a POST request

---
+++ src/redux/slices/reservations/createReservationSlice.js
@@ -0,0 +1,45 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const BASE_URL = 'http://localhost:3000/api/v1';
+
+export const createReservation = createAsyncThunk(
+  'reservations/createReservation',
+  async (newReservation) => {
+    const response = await axios.post(`${BASE_URL}/reservations`, { reservation: newReservation }, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    return response.data;
+  },
+);
+
+const initialState = {
+  reservation: [],
+  loading: false,
+  error: null,
+};
+
+const createReservationSlice = createSlice({
+  name: 'createReservation',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(createReservation.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(createReservation.fulfilled, (state, action) => {
+        state.loading = false;
+        state.reservation = action.payload;
+      })
+      .addCase(createReservation.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message;
+      });
+  },
+});
+
+export default createReservationSlice.reducer;
